98cd6eb5c3b4cf8e78933a6f43b79e5806efb198
jasur <zasur4726@gmail.com>
2026-05-22 17:06:31 +0500

    Add files via upload

---
+++ 2-dars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+cout<<"SALOM"<<end
+int main () {
+int a= 4;
+int b=6;
+int c=a+b;
+cout<< "natija-"<<c;
+
+}
+
+
---
+++ text (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main {
+
+int son;
+cout << "son kiriting: "
+cin >> son;
+if (son == 10) {
+    }
+}
